0f87e1c027a699642067d3e31545d6b81f8d36b8
“AkashBalasubramani-Router <akashb@routerprotocol.com>
2023-12-10 01:14:35 +0530

    Libraries and scripts

---
+++ tasks/interact.ts
@@ -0,0 +1,41 @@
+import { task } from "hardhat/config";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { parseEther } from "@ethersproject/units";
+import { getAddress } from "@zetachain/protocol-contracts";
+import { prepareData } from "@zetachain/toolkit/helpers";
+import { getNetwork } from "@ethersproject/providers";
+
+const main = async (args: any, hre: HardhatRuntimeEnvironment) => {
+  const [signer] = await hre.ethers.getSigners();
+
+  // const data = prepareData(args.contract, ["uint8"], ["1"]);
+  const data = prepareData(
+    args.contract,
+    ["uint8", "address"],
+    ["3", args.beneficiary]
+  );
+
+  const to = getAddress("tss", hre.network.name);
+  // const value = parseEther(args.amount);
+
+  const tx = await signer.sendTransaction({ data, to });
+
+  if (args.json) {
+    console.log(JSON.stringify(tx, null, 2));
+  } else {
+    console.log(`🔑 Using account: ${signer.address}\n`);
+
+    console.log(`🚀 Successfully broadcasted a token transfer transaction on ${hre.network.name} network.
+📝 Transaction hash: ${tx.hash}
+`);
+  }
+};
+
+// task("interact", "Interact with the contract", main)
+//   .addParam("contract", "The address of the withdraw contract on ZetaChain")
+//   .addParam("amount", "Amount of tokens to send")
+//   .addFlag("json", "Output in JSON");
+task("interact", "Interact with the contract", main)
+  .addParam("beneficiary", "The address of the withdraw contract on ZetaChain")
+  .addParam("contract", "Amount of tokens to send")
+  .addFlag("json", "Output in JSON");
